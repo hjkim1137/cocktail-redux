8f17de37183bfb358071dcd40be2f2fd872dd76d
Hyunjung Kim <withcusco@gmail.com>
2023-09-26 15:05:49 +0900

    [remove] 불필요한 파일 삭제

--- src/features/weatherSlice.ts
+++ src/features/weatherSlice.ts
@@ -1,18 +1,5 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { fetchWeatherData, getLocation } from '../API/WeatherAPI/index';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { WeatherType, WeatherState } from './weatherSlice.interface';
-
-const fetchWeather = createAsyncThunk('weather/fetchWeather', async () => {
-  try {
-    const position = await getLocation();
-    const { latitude, longitude } = position.coords;
-    const weatherData = await fetchWeatherData(latitude, longitude);
-    return weatherData;
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
-  }
-});
 
 // null 체크
 const initialState: WeatherState = {
