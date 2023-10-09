81554cf77cf800c14d862c1f80de0bb4c4f9bd6b
Hyunjung Kim <withcusco@gmail.com>
2023-10-09 09:48:17 +0900

    [omment] 코드 주석 수정

--- src/components/Home/getCocktail/GetCocktail.tsx
+++ src/components/Home/getCocktail/GetCocktail.tsx
@@ -8,19 +8,19 @@ import { RootState } from '../../../store/store';
 import { AppDispatch } from '../../../store/store';
 
 function GetCocktail({ weatherName }: GetCocktailProps) {
-  // 트러블 슈팅: const dispatch = useDispatch<AppDispatch>(); 이렇게 명시 안하면
+  // [트러블 슈팅]
+  // const dispatch = useDispatch<AppDispatch>(); 이렇게 명시 안하면
   // 아래 dispatch(fetchCocktail(weatherName)); 부분에서
-  // 'AsyncThunkAction<Cocktail, string, AsyncThunkConfig>' 형식의 인수는
-  // 'AnyAction' 형식의 매개 변수에 할당될 수 없습니다. 오류 발생
-  // 이것은 기본적으로 dispatch가 기대하는 액션의 형태와 fetchCocktail(weatherName)에서
-  // 반환되는 액션의 형태가 일치하지 않다는 것을 의미합니다.
+  // 'AsyncThunkAction<Cocktail, string, AsyncThunkConfig>' 형식의 인수는'AnyAction' 형식의 매개 변수에 할당될 수 없다는 오류발생
 
-  // 여기서 문제가 발생하는 원인:
-  // Redux의 store.dispatch 메서드는 기본적으로 Redux의 AnyAction 타입을 기대합니다.
-  // redux-thunk 라이브러리를 사용할 때, 비동기 작업을 처리하는 thunk action creators는
-  // AsyncThunkAction이라는 특수한 타입의 액션을 반환합니다. 이 타입의 액션은 AnyAction 타입과 다릅니다.
-  // 이 문제를 해결하는 방법은 useDispatch 훅에서 반환되는 dispatch 함수의 타입을 명시적으로 AppDispatch로 지정하는 것입니다.
-  // 이렇게 하면, dispatch 함수는 thunk action creators에서 반환되는 액션들도 처리할 수 있게 됩니다.
+  // [문제 원인]
+  // Redux의 store.dispatch 메서드는 기본적으로 Redux의 'AnyAction' 타입을 기대합니다.
+  // 반면 비동기 작업을 처리하는 thunk action creators는 'AsyncThunkAction'이라는 특수한 타입의 액션을 반환합니다.
+  // 때문에 기대되는 반환 값이 달라 발생하는 문제임
+
+  // [해결 방법]
+  // useDispatch 훅에서 반환되는 dispatch 함수의 타입을 명시적으로 AppDispatch로 지정하는 것
+  // 이렇게 하면, dispatch 함수는 thunk action creators에서 반환되는 액션들도 처리할 수 있게 됨
 
   const dispatch = useDispatch<AppDispatch>();
   const { cocktailInfo, status } = useSelector(
@@ -29,7 +29,7 @@ function GetCocktail({ weatherName }: GetCocktailProps) {
 
   useEffect(() => {
     if (weatherName) {
-      dispatch(fetchCocktail(weatherName));
+      dispatch(fetchCocktail(weatherName)); // fetchCocktail은 비동기 작업
     }
   }, [dispatch, weatherName]);
 
