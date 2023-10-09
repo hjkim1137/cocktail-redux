886064626115d85ef82f20738f888df798fa3b9e
Hyunjung Kim <withcusco@gmail.com>
2023-10-09 11:19:56 +0900

    [docs] application/json 인지 확인하기

--- src/features/cocktailSlice.ts
+++ src/features/cocktailSlice.ts
@@ -5,14 +5,34 @@ import { CocktailState, Cocktail } from './cocktailSlice.interface';
 export const fetchCocktail = createAsyncThunk<Cocktail, string>(
   'cocktail/fetchCocktail',
   async (weatherName) => {
-    const glassType: any = WGobject[weatherName];
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glassType}`
-    );
-    const data = await response.json();
-    const cocktailArr = data.drinks;
-    const randomIndex = Math.floor(Math.random() * cocktailArr.length);
-    return cocktailArr[randomIndex];
+    try {
+      const glassType: any = WGobject[weatherName];
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glassType}`
+      );
+      console.log('response:', response.headers.get('content-type')); // 오류라면: text/html 반환
+      if (!response.ok) {
+        throw new Error(
+          `칵테일 데이터를 가져오는 데 실패했습니다: ${response.statusText}`
+        );
+      }
+      // 트러블 슈팅:
+      // 오류가 발생했습니다: SyntaxError: Unexpected token '<', "<br /> <b>"... is not valid JSON
+      // [해결방법] 응답이 JSON 형식인지 확인함
+      if (response.headers.get('content-type')?.includes('application/json')) {
+        const data = await response.json();
+        const cocktailArr = data.drinks;
+        const randomIndex = Math.floor(Math.random() * cocktailArr.length);
+        return cocktailArr[randomIndex];
+      } else {
+        console.error(
+          '서버로부터 예상치 못한 응답을 받았습니다. 나중에 다시 시도해 주세요.'
+        );
+      }
+    } catch (error) {
+      console.error('오류가 발생했습니다:', error);
+      throw error;
+    }
   }
 );
 
